16aec35b42ad3a64668747cde647d6de5ede111f
Jhoan Gutierrez <jhoangutierrezc@gmail.com>
2024-02-19 12:22:28 -0500

    test up

---
+++ 01-dart-intro/1-hello-world.dart
@@ -0,0 +1,9 @@
+void main() {
+  //var myName = 'jhoan';
+  String myName = 'jhoan';
+  //final myName = 'jhoan';
+  myName = 'Javier';
+  print('jeje ${myName.toUpperCase()}');
+  print(1 + 1);
+  print('jeje ${1+1}');
+}
--- Curso/01-dart-intro/1-hello-world.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  //var myName = 'jhoan';
-  String myName = 'jhoan';
-  //final myName = 'jhoan';
-  myName = 'Javier';
-  print('jeje ${myName.toUpperCase()}');
-  print(1 + 1);
-  print('jeje ${1+1}');
-}
